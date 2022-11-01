6bfa682382b00617f304b8e9c72ae701c792d7ba
SangHee Lee <lsh277604@gmail.com>
2022-11-01 11:15:16 +0900

    psh2

---
+++ 1101/1101.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#define MAXARGS 20
+#define ARGLEN 100
+
+
+void execute(char** );
+char* makestring();
+int main(int argc, char* argv[]){
+    char *arglist[MAXARGS + 1];
+    int numargs;
+    char argbuf[ARGLEN];
+
+    // numargs = 0;
+    // while(numargs < MAXARGS){
+    //     printf("Sang Arg[%d}? ", numargs);
+    //     if( fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+    //         arglist[numargs++] = makestring(argbuf);
+    //     else{
+    //             if(numargs > 0){
+    //                 arglist[numargs] = NULL;
+    //                 execute( arglist );
+    //                 numargs = 0;
+    //             }
+    //     }
+    // }
+    for(numargs = 0; numargs<MAXARGS ; numargs++){
+        printf("Sang Arg[%d}? ", numargs);
+        if( fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+            arglist[numargs] = makestring(argbuf);
+        else{
+                if(numargs > 0){
+                    arglist[numargs] = NULL;
+                    execute( arglist );
+                    numargs = -1;
+                }
+        }
+    }
+    return 0;
+}
+
+void execute(char *arglist[]){
+    int pid, exitstatus;
+    pid = fork();
+    switch (pid)
+    {
+        case -1:
+            perror("fork failed.");
+            exit(1);
+        case 0:
+            execvp(arglist[0], arglist);
+            perror("execvp failed.");
+            exit(1);
+        default:
+            while( wait(&exitstatus) != pid);
+            printf("child exited with status %d, %d\n", exitstatus>>8, exitstatus&0377);
+    }
+}
+
+char* makestring(char *buf){
+    char *cp;
+    buf[strlen(buf)-1] = '\0';
+    cp = malloc(strlen(buf) + 1);
+    if( cp == NULL){
+        fprintf(stderr, "no memory\n");
+        exit(1);
+    }
+    strcpy(cp, buf);
+    return cp;
+}
